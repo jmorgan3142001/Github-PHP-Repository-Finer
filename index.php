2beeaa82fc0a0e099019bf5c540c04880464383e
Jake Morgan <jmorgan3142001@gmail.com>
2023-07-07 15:07:27 -0500

    Link update and removal of unused variable

--- index.php
+++ index.php
@@ -15,9 +15,6 @@
 
 require 'DBConnect.php';
 
-$url_request = explode("/", $_SERVER['REQUEST_URI']);
-$url = $url_request[0];
-
 echo "<h1 class='bg-dark text-light w-50 m-3 p-3 mx-auto text-center rounded'>Top <u>100</u> Public PHP Github Repositories</h2>";
 echo "<h5 class='mx-auto w-25 border border-dark p-1 m-3 text-center'>Click <a href='".$url."/db_refresh.php'><u>HERE</u></a> to refresh the database</h5>";
 
@@ -30,7 +27,7 @@ if($query_data == false) die('DB Error: '.$mysqli->error);
 echo "<table border='1' class='table table-striped table-sm w-25 mx-auto'>";
 echo "<thead class='thead-dark'><tr><th scope='col'>Rank</th><th scope='col'>Name</th><th scope='col'>&nbsp# of Stars&nbsp</th></tr></thead>";
 while ($row = $query_data->fetch_assoc()) {
-    echo "<tr><th scope='row'>".($row['id']+1)."</th><td><a href='".$url."detail.php/?id=".$row['id']."'>".$row['name']."</a></td><td>".$row['stars']."</td></tr>";
+    echo "<tr><th scope='row'>".($row['id']+1)."</th><td><a href='detail.php/?id=".$row['id']."'>".$row['name']."</a></td><td>".$row['stars']."</td></tr>";
 }
 echo "</table>";
 
